18effdaccc0544688f9dcdc089b29651c2ee2df2
Sean Hsu <hsuknowledge@gmail.com>
2026-04-18 20:55:47 +0800

    Optimize for 128-bit vload and RDNA3 dual-issuing - Design grid size to fit in one WGP = 2 CU = 128 SIMD lanes - Compensated summation for sensitive and critical log-likelihoods - Fuse 4 log calls into 1 to improve speed without losing accuracy - Increase batch size to 50k pairs (200KB) ~= C(316, 2) = 49770 - Heads up: CUDA GPUs might encounter frequent numerical errors!

--- src/SanityDistanceGPU.jl
+++ src/SanityDistanceGPU.jl
@@ -1,101 +1,106 @@
+using Base.Cartesian: @nexprs
 using Combinatorics
 using KernelAbstractions
 
-@inline mymax(x, y) = x > y ? x : y
-
-@kernel cpu=false inbounds=true function celldistance_256bins!(
-        @Const(pairs), @Const(delta), @Const(epsilon), @Const(vg), @Const(alpha), dist_out
-    )
-    p, a = @index(Global, NTuple) # pair & alpha indices
-    ltmp = @localmem Float32 256
-    dtmp = @localmem Float32 256
-    i1, i2 = pairs[p] >>> 32, pairs[p] & 0xffffffff
-    @private acc_ll = 0f0
-    @private acc_dd = 0f0
-    for g in 1:size(vg)[1]
-        x2 = abs2(delta[g, i1] - delta[g, i2])
-        e2 = epsilon[g, i1] + epsilon[g, i2]
-        prior = inv(alpha[a] * vg[g] + e2)
-        acc_ll += x2 * prior - log(prior)
-        shrinker = alpha[a] * vg[g] * prior
-        acc_dd += (x2 * shrinker + e2) * shrinker
-    end
-    acc_ll = reinterpret(Float32, reinterpret(UInt32, acc_ll) + 0x7f800000) # / -2
-    ltmp[a] = acc_ll
-    @synchronize()
-    if a <= 128 ltmp[a] = mymax(ltmp[a], ltmp[a + 128]) end
-    @synchronize()
-    if a <= 64  ltmp[a] = mymax(ltmp[a], ltmp[a + 64])  end
-    @synchronize()
-    if a <= 32  ltmp[a] = mymax(ltmp[a], ltmp[a + 32])  end
-    @synchronize()
-    if a <= 16  ltmp[a] = mymax(ltmp[a], ltmp[a + 16])  end
-    @synchronize()
-    if a <= 8   ltmp[a] = mymax(ltmp[a], ltmp[a + 8])   end
-    @synchronize()
-    if a <= 4   ltmp[a] = mymax(ltmp[a], ltmp[a + 4])   end
-    @synchronize()
-    if a <= 2   ltmp[a] = mymax(ltmp[a], ltmp[a + 2])   end
-    @synchronize()
-    if a <= 1   ltmp[a] = mymax(ltmp[a], ltmp[a + 1])   end
-    @synchronize()
-    ltmp[a] = acc_ll = exp(acc_ll - ltmp[1])
-    dtmp[a] = acc_ll * acc_dd
-    @synchronize()
-    if a <= 128
-        ltmp[a] = ltmp[a] + ltmp[a + 128]
-        dtmp[a] = dtmp[a] + dtmp[a + 128]
-    end
-    @synchronize()
-    if a <= 64
-        ltmp[a] = ltmp[a] + ltmp[a + 64]
-        dtmp[a] = dtmp[a] + dtmp[a + 64]
-    end
-    @synchronize()
-    if a <= 32
-        ltmp[a] = ltmp[a] + ltmp[a + 32]
-        dtmp[a] = dtmp[a] + dtmp[a + 32]
-    end
-    @synchronize()
-    if a <= 16
-        ltmp[a] = ltmp[a] + ltmp[a + 16]
-        dtmp[a] = dtmp[a] + dtmp[a + 16]
-    end
-    @synchronize()
-    if a <= 8
-        ltmp[a] = ltmp[a] + ltmp[a + 8]
-        dtmp[a] = dtmp[a] + dtmp[a + 8]
-    end
-    @synchronize()
-    if a <= 4
-        ltmp[a] = ltmp[a] + ltmp[a + 4]
-        dtmp[a] = dtmp[a] + dtmp[a + 4]
-    end
-    @synchronize()
-    if a <= 2
-        ltmp[a] = ltmp[a] + ltmp[a + 2]
-        dtmp[a] = dtmp[a] + dtmp[a + 2]
-    end
-    @synchronize()
-    if a <= 1
-        ltmp[a] = ltmp[a] + ltmp[a + 1]
-        dtmp[a] = dtmp[a] + dtmp[a + 1]
-    end
-    @synchronize()
-    dist_out[p] = sqrt(dtmp[1] / ltmp[1])
+macro kbn_sum!(acc, x, c) # Kahan-Babuska-Neumaier method (ref. KahanSummation.jl)
+    esc(quote
+        t = $acc + $x
+        $c -= ($acc - t) + $x # assuming |acc| > |x| && acc is strictly increasing
+        $acc = t
+    end)
 end
 
-function cell_distance!(dist_out, del_gpu, eps_gpu, vg_gpu; batchsize = 10000)
+@kernel cpu=false inbounds=true function celldistance_256bins!(
+        @Const(pairs), @Const(del), @Const(eta), @Const(vg), @Const(alpha), dist_out
+    )
+    p, a = @index(Global, NTuple) # pair & alpha indices
+    ltmp = @localmem Float32 128
+    dtmp = @localmem Float32 128
+    i1, i2 = pairs[p] >>> 32, pairs[p] & 0xffffffff
+    ng = size(vg)[1]
+    @private acc_ll1 = 0f0
+    @private acc_ll2 = 0f0
+    @private acc_dd1 = 0f0
+    @private acc_dd2 = 0f0
+    cll1 = cll2 = 0f0
+    for g in 1:fld(ng, 4)
+        @nexprs 4 j->begin
+            g_j = 4g - 4 + j
+            x2_j = abs2(del[g_j, i1] - del[g_j, i2])
+            e2_j = eta[g_j, i1] + eta[g_j, i2]
+            av1_j = alpha[2a-1] * vg[g_j]
+            av2_j = alpha[2a] * vg[g_j]
+            p1_j = inv(av1_j + e2_j)
+            p2_j = inv(av2_j + e2_j)
+            @kbn_sum!(acc_ll1, x2_j * p1_j, cll1)
+            @kbn_sum!(acc_ll2, x2_j * p2_j, cll2)
+            acc_dd1 += (x2_j * p1_j * av1_j + e2_j) * av1_j * p1_j
+            acc_dd2 += (x2_j * p2_j * av2_j + e2_j) * av2_j * p2_j
+        end
+        @kbn_sum!(acc_ll1, -log(p1_1 * p1_2 * p1_3 * p1_4), cll1)
+        @kbn_sum!(acc_ll2, -log(p2_1 * p2_2 * p2_3 * p2_4), cll2)
+    end
+    for g in (fld(ng, 4)*4+1):ng
+        x2 = abs2(del[g, i1] - del[g, i2])
+        e2 = eta[g, i1] + eta[g, i2]
+        av1, av2 = alpha[2a-1] * vg[g], alpha[2a] * vg[g]
+        p1 = inv(av1 + e2)
+        p2 = inv(av2 + e2)
+        @kbn_sum!(acc_ll1, x2 * p1, cll1)
+        @kbn_sum!(acc_ll2, x2 * p2, cll2)
+        acc_dd1 += (x2 * p1 * av1 + e2) * av1 * p1
+        acc_dd2 += (x2 * p2 * av2 + e2) * av2 * p2
+        @kbn_sum!(acc_ll1, -log(p1), cll1)
+        @kbn_sum!(acc_ll2, -log(p2), cll2)
+    end
+    acc_ll1 = (acc_ll1 - cll1) * -0.5f0
+    acc_ll2 = (acc_ll2 - cll2) * -0.5f0
+    ltmp[a] = max(acc_ll1, acc_ll2)
+    @synchronize()
+    if a % 8 == 1
+        m1 = max(ltmp[a], ltmp[a + 1])
+        m2 = max(ltmp[a + 2], ltmp[a + 3])
+        m3 = max(ltmp[a + 4], ltmp[a + 5])
+        m4 = max(ltmp[a + 6], ltmp[a + 7])
+        ltmp[a] = max(m1, m2, m3, m4)
+    end
+    @synchronize()
+    if a % 64 == 1
+        m1 = max(ltmp[a], ltmp[a + 8])
+        m2 = max(ltmp[a + 16], ltmp[a + 24])
+        m3 = max(ltmp[a + 32], ltmp[a + 40])
+        m4 = max(ltmp[a + 48], ltmp[a + 56])
+        ltmp[a] = max(m1, m2, m3, m4)
+    end
+    @synchronize()
+    lmax = max(ltmp[1], ltmp[65])
+    acc_ll1 = exp(acc_ll1 - lmax)
+    acc_ll2 = exp(acc_ll2 - lmax)
+    ltmp[a] = acc_ll1 + acc_ll2
+    dtmp[a] = acc_ll1 * acc_dd1 + acc_ll2 * acc_dd2
+    @synchronize()
+    @nexprs 6 j->begin
+        if a % 2^j == 1 # a % {2, 4, 8, 16, 32, 64}
+            ltmp[a] = ltmp[a] + ltmp[a + 2^(j-1)] # [a] + [a + {1,2,4,8,16,32}]
+            dtmp[a] = dtmp[a] + dtmp[a + 2^(j-1)]
+        end
+        @synchronize()
+    end
+    if a == 1
+        dist_out[p] = sqrt((dtmp[1] + dtmp[65]) / (ltmp[1] + ltmp[65]))
+    end
+end
+
+function cell_distance!(dist_out, del_gpu, eta_gpu, vg_gpu; batchsize = 50000)
     comb, s = combinations(1:size(dist_out)[2], 2), [1, 1]
-    lastsize = batchsize
-    nb = 256
+    lastsize = batchsize = min(batchsize, length(comb))
     pairs = Vector{Int}(undef, batchsize)
     idx = Vector{CartesianIndex{2}}(undef, batchsize)
     backend = get_backend(vg_gpu)
-    alpha_gpu = allocate(backend, Float32, nb)
-    copyto!(alpha_gpu, collect(1:nb) ./ (nb / 2))
+    alpha_gpu = allocate(backend, Float32, 256)
+    copyto!(alpha_gpu, collect(1:256) ./ 128)
     pairs_gpu = allocate(backend, Int, batchsize)
-    k1! = celldistance_256bins!(backend, (1, nb))
+    k1! = celldistance_256bins!(backend, (1, 128))
     dist_gpu = allocate(backend, Float32, batchsize)
     dist_tmp_cpu = Vector{Float32}(undef, batchsize)
     @time while !isnothing(s)
@@ -112,9 +117,20 @@ function cell_distance!(dist_out, del_gpu, eps_gpu, vg_gpu; batchsize = 10000)
         if lastsize == 0 break end
         ## begin GPU region
         copyto!(pairs_gpu, pairs)
-        k1!(pairs_gpu, del_gpu, eps_gpu, vg_gpu, alpha_gpu, dist_gpu; ndrange = (lastsize, nb))
+        k1!(pairs_gpu, del_gpu, eta_gpu, vg_gpu, alpha_gpu, dist_gpu; ndrange = (lastsize, 128))
         copyto!(dist_tmp_cpu, dist_gpu)
         ## end GPU region
         dist_out[idx[1:lastsize]] .= dist_tmp_cpu[1:lastsize]
+    end
+    idx = findall(.!(isfinite.(dist_out))) # reevaluate any nan or inf result
+    np = length(idx)
+    if np > 0
+        pairs = [x[1] << 32 | x[1] for x in idx]
+        resize!(dist_gpu, np)
+        resize!(dist_tmp_cpu, np)
+        copyto!(pairs_gpu, pairs)
+        k1!(pairs_gpu, del_gpu, eta_gpu, vg_gpu, alpha_gpu, dist_gpu; ndrange = (np, 128))
+        copyto!(dist_tmp_cpu, dist_gpu)
+        dist_out[idx] .= dist_tmp_cpu
     end
 end
